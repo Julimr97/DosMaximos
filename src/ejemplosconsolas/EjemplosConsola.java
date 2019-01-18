f3811e0058d1686ed6273bcb5f14ff739eac74a3
juli <juli@10.10.35.19>
2019-01-18 12:27:43 +0100

    Autor: Julián Mora Programa que calcula los dos maximos numeros de una lista

--- src/ejemplosconsolas/EjemplosConsola.java
+++ src/ejemplosconsolas/EjemplosConsola.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
  * @author juli
  */
 public class EjemplosConsola {
-    int[]listaNumeros =	{20,31,27,2,5,99};
+    int[]listaNumeros =	{203, 20, 8, 9, 201};
 
     /**
      * @param args the command line arguments
@@ -22,13 +22,23 @@ public class EjemplosConsola {
     El metodo maximos va a calcular el maximo y el segundo maximo de la lista
     de numeros
     */
-public int[] maximos (int[] lista){
-    int [] listaMaximos = {lista [0], lista[0]};
+public int[] maximos (int[] lista){ 
+  
+
+    int [] listaMaximos = {lista [0], lista[1]};
     for (int i=0; i< lista.length ; i++){
-         if (listaMaximos [0] < lista[i]){
+        
+         if (listaMaximos [0] <= lista[i]){
+             if (i!=0)
              listaMaximos[1] = listaMaximos[0];
             listaMaximos[0] = lista[i];
         }
+         else{if (lista [i] >= listaMaximos[1]){
+             listaMaximos[1] = lista[i];
+             
+        }
+             
+         }
     }
     
             return listaMaximos;
